d6ccda3a266cd2efd52883d455c41b3e4219a762
Qingshan Luo <edoger@qq.com>
2020-11-15 21:36:15 +0800

    Add documents and comments.

--- configurator.go
+++ configurator.go
@@ -28,37 +28,70 @@ var (
 	ErrNotFound = errors.New("configurator: not found")
 )
 
+// Configurator defines the configuration manager.
 type Configurator interface {
+	// Use registers a custom configuration loader.
+	// The last registered configuration loader will have the highest priority.
 	Use(Loader) Configurator
 
+	// AddFile adds one or more config files to the current loader.
+	// The given parameter need to comply with the search rules supported
+	// by filepath.Glob.
+	// This method comes from the built-in configuration file loader.
 	AddFile(string) error
 
+	// Load loads the given config target.
+	// If the given config target does not exist, ErrNotFound is returned.
+	// We will give priority to the custom loader. If there is no available config loader
+	// or all registered config loaders cannot load the config target (the semantic target
+	// does not exist), it will be automatically delegated to the built-in config file loader.
 	Load(string) (Item, error)
+
+	// LoadJSON loads the given config target and binds it to the given object as json.
 	LoadJSON(string, interface{}) error
+
+	// LoadXML loads the given config target and binds it to the given object as xml.
 	LoadXML(string, interface{}) error
+
+	// LoadTOML loads the given config target and binds it to the given object as toml.
 	LoadTOML(string, interface{}) error
+
+	// LoadYAML loads the given config target and binds it to the given object as yaml.
 	LoadYAML(string, interface{}) error
 }
 
+// New creates and returns a new Configurator instance.
 func New() Configurator {
 	fs := newFileLoader()
 	return &configurator{fs: fs, loaders: []Loader{fs}}
 }
 
+// The configurator type is a built-in implementation of the Configurator interface.
 type configurator struct {
-	fs *fileLoader
+	fs      *fileLoader
 	loaders []Loader
 }
 
+// Use registers a custom configuration loader.
+// The last registered configuration loader will have the highest priority.
 func (o *configurator) Use(loader Loader) Configurator {
 	o.loaders = append(o.loaders, loader)
 	return o
 }
 
+// AddFile adds one or more config files to the current loader.
+// The given parameter need to comply with the search rules supported
+// by filepath.Glob.
+// This method comes from the built-in configuration file loader.
 func (o *configurator) AddFile(pattern string) error {
 	return o.fs.AddFile(pattern)
 }
 
+// Load loads the given config target.
+// If the given config target does not exist, ErrNotFound is returned.
+// We will give priority to the custom loader. If there is no available config loader
+// or all registered config loaders cannot load the config target (the semantic target
+// does not exist), it will be automatically delegated to the built-in config file loader.
 func (o *configurator) Load(target string) (Item, error) {
 	for k := len(o.loaders) - 1; k >= 0; k-- {
 		if item, err := o.loaders[k].Load(target); err == nil {
@@ -74,6 +107,7 @@ func (o *configurator) Load(target string) (Item, error) {
 	return nil, ErrNotFound
 }
 
+// LoadJSON loads the given config target and binds it to the given object as json.
 func (o *configurator) LoadJSON(target string, v interface{}) error {
 	if item, err := o.Load(target); err != nil {
 		return err
@@ -82,6 +116,7 @@ func (o *configurator) LoadJSON(target string, v interface{}) error {
 	}
 }
 
+// LoadXML loads the given config target and binds it to the given object as xml.
 func (o *configurator) LoadXML(target string, v interface{}) error {
 	if item, err := o.Load(target); err != nil {
 		return err
@@ -90,6 +125,7 @@ func (o *configurator) LoadXML(target string, v interface{}) error {
 	}
 }
 
+// LoadTOML loads the given config target and binds it to the given object as toml.
 func (o *configurator) LoadTOML(target string, v interface{}) error {
 	if item, err := o.Load(target); err != nil {
 		return err
@@ -98,6 +134,7 @@ func (o *configurator) LoadTOML(target string, v interface{}) error {
 	}
 }
 
+// LoadYAML loads the given config target and binds it to the given object as yaml.
 func (o *configurator) LoadYAML(target string, v interface{}) error {
 	if item, err := o.Load(target); err != nil {
 		return err
